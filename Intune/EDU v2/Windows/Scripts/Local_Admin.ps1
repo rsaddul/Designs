1680d47cb22504869362a63969fc72b8804e5c41
rsaddul <52894448+rsaddul@users.noreply.github.com>
2024-01-04 10:11:03 +0000

    Add files via upload

---
+++ Intune/EDU v2/Windows/Scripts/Local_Admin.ps1
@@ -0,0 +1,13 @@
+﻿$userName = "MCSAdmin"
+$Password = ConvertTo-SecureString "BLF&sax9(%k4L8" -AsPlainText -Force
+$Userexist = (Get-LocalUser).Name -Contains $userName
+if($userexist -eq $false) {
+  try{ 
+     New-LocalUser -Name $username -password $Password -Description "Local Workstation Admin Account" -PasswordNeverExpires -ErrorAction SilentlyContinue
+     Add-LocalGroupMember -Group "Administrators" -Member $userName -ErrorAction SilentlyContinue
+     Exit 0
+   }   
+  Catch {
+     Exit 1
+   }
+}

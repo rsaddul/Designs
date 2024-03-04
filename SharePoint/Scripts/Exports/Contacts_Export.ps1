d1c0bc802c5791248546ff4fe32d41e9ce0b1b78
rsaddul <52894448+rsaddul@users.noreply.github.com>
2024-03-04 12:21:30 +0000

    Add files via upload

---
+++ SharePoint/Scripts/Exports/Contacts_Export.ps1
@@ -0,0 +1,33 @@
+﻿<#
+Developed by: Rhys Saddul
+#>
+
+
+# Install and import the ExchangeOnlineManagement module if not already installed
+if (-not (Get-Module ExchangeOnlineManagement -ErrorAction SilentlyContinue)) {
+    Install-Module -Name ExchangeOnlineManagement -Force -AllowClobber
+}
+Import-Module ExchangeOnlineManagement
+
+# Prompt to ensure varibles have been setup correctly prior to running the script
+$dialogResult = [System.Windows.Forms.MessageBox]::Show("Have you set the varible for the output file path?", "Variable Setup", "YesNo", "Question")
+if ($dialogResult -eq "Yes") {
+    Write-Host "User confirmed variables have been set. Proceeding with the script." -ForegroundColor Green
+} else {
+    Write-Host "User confirmed Variables have not been set. Exiting the script." -ForegroundColor Red
+    return
+}
+
+# Varible for the output file path
+$ExportPath = "C:\Users\RhysSaddul\OneDrive - eduthing\Documents\BFLT\365_Contacts.csv"
+
+# Connect to Exchange Online with progress display
+Connect-ExchangeOnline -ShowProgress $true -ShowBanner:$false
+
+# Retrieve guest contacts with specified properties and export them to CSV file
+Get-Contact -RecipientTypeDetails MailContact | 
+    Select-Object DisplayName, FirstName, LastName, WindowsEmailAddress, RecipientType, DistinguishedName, IsDirSynced | 
+    Export-Csv -Path $ExportPath -NoTypeInformation
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline -Confirm:$false

68421917ec3ba9e191dd92ad4265b7a98fc8ce70
rsaddul <52894448+rsaddul@users.noreply.github.com>
2024-03-06 12:28:09 +0000

    Add files via upload

---
+++ SharePoint/Scripts/Exports/Users/Guests_Export.ps1
@@ -0,0 +1,54 @@
+﻿<#
+Developed by: Rhys Saddul
+#>
+
+# Install and import the Microsoft.Graph.Users module if not already installed
+# Check if the module is installed
+if (-not (Get-Module -Name Microsoft.Graph.Users -ListAvailable)) {
+    # Install the module
+    Install-Module -Name Microsoft.Graph.Users -Scope CurrentUser -Force
+}
+Import-Module ExchangeOnlineManagement
+
+# Prompt to ensure varibles have been setup correctly prior to running the script
+$dialogResult = [System.Windows.Forms.MessageBox]::Show("Have you set the varible for the output file path?", "Variable Setup", "YesNo", "Question")
+if ($dialogResult -eq "Yes") {
+    Write-Host "User confirmed variables have been set. Proceeding with the script." -ForegroundColor Green
+} else {
+    Write-Host "User confirmed Variables have not been set. Exiting the script." -ForegroundColor Red
+    return
+}
+
+# Varible for the output file path
+$ExportPath = "C:\Users\RhysSaddul\OneDrive - eduthing\Documents\Exports\Users\Guests_Export.csv"
+
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes 'User.Read.All' -NoWelcome | Out-Null
+
+# Retrieve Office 365 guests and their IDs
+$GuestUserIds = Get-MgUser -All -Filter "UserType eq 'Guest'" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Id
+
+# Initialize an empty array to store guest user details
+$GuestUsers = @()
+
+# Retrieve detailed information for each guest user
+foreach ($userId in $GuestUserIds) {
+    $userDetail = Get-MgUser -UserId $userId
+    $GuestUsers += [PSCustomObject]@{
+        GivenName = $userDetail.GivenName
+        Surname = $userDetail.Surname
+        DisplayName = $userDetail.DisplayName
+        Mail = $userDetail.Mail
+        UserPrincipalName = $userDetail.UserPrincipalName
+        Id = $userDetail.Id
+    }
+}
+
+# Export guest user details to CSV file with UTF-8 encoding without BOM
+$GuestUsers | Export-Csv -Path $ExportPath -Encoding UTF8 -NoTypeInformation
+
+# Disconnect from Microsoft Graph
+Disconnect-MgGraph | Out-Null
+
+# Check if any of the guest users have empty or null
+# $GuestUsers | Where-Object { $_.GivenName -eq $null -or $_.GivenName -eq '' -or $_.Surname -eq $null -or $_.Surname -eq '' }
---
+++ SharePoint/Scripts/Exports/Users/Users_Export.ps1
@@ -0,0 +1,68 @@
+﻿<#
+Developed by: Rhys Saddul
+#>
+
+# Install and import the ExchangeOnlineManagement module if not already installed
+if (-not (Get-Module ExchangeOnlineManagement -ErrorAction SilentlyContinue)) {
+    Install-Module -Name ExchangeOnlineManagement -Force -AllowClobber
+}
+Import-Module ExchangeOnlineManagement
+
+# Prompt to ensure varibles have been setup correctly prior to running the script
+$dialogResult = [System.Windows.Forms.MessageBox]::Show("Have you set the varible for the output file path?", "Variable Setup", "YesNo", "Question")
+if ($dialogResult -eq "Yes") {
+    Write-Host "User confirmed variables have been set. Proceeding with the script." -ForegroundColor Green
+} else {
+    Write-Host "User confirmed Variables have not been set. Exiting the script." -ForegroundColor Red
+    return
+}
+
+# Varible for the output file path
+$outputFilePath = "C:\Users\RhysSaddul\OneDrive - eduthing\Documents\Exports\Users\Users_Export.csv"
+
+# Connect to Exchange Online with progress display
+Connect-ExchangeOnline -ShowProgress $true -ShowBanner:$false
+
+# Get all mailbox users
+$mailboxes = Get-ExoMailbox
+
+# Initialize an array to store mailbox information
+$mailboxInfo = @()
+
+# Iterate through each mailbox
+# Iterate through each mailbox
+foreach ($mailbox in $mailboxes) {
+    # Check if the RecipientTypeDetails is UserMailbox
+    if ($mailbox.RecipientTypeDetails -eq "UserMailbox") {
+        # Create an object to store mailbox information
+        $mailboxObject = [PSCustomObject]@{
+            DisplayName = $mailbox.DisplayName
+            Alias = $mailbox.Alias
+            PrimarySmtpAddress = $mailbox.PrimarySmtpAddress
+            RecipientType = $mailbox.RecipientTypeDetails
+        }
+
+        # Initialize an array to store email addresses
+        $emailAddresses = @()
+
+        # Add each email address to the array
+        foreach ($emailAddress in $mailbox.EmailAddresses) {
+            $emailAddresses += $emailAddress
+        }
+
+        # Add the email addresses as a property to the mailbox object
+        $mailboxObject | Add-Member -MemberType NoteProperty -Name EmailAddresses -Value ($emailAddresses -join ";")
+
+        # Add the mailbox object to the array
+        $mailboxInfo += $mailboxObject
+    }
+}
+
+# Export mailbox information to CSV
+$mailboxInfo | Export-Csv -Path $outputFilePath -NoTypeInformation
+
+# Disconnect from Exchange Online
+Disconnect-ExchangeOnline -Confirm:$false
+
+
+Get-ExoMailbox vdeoliveria@kfos.co.uk | Select-Object EmailAddresses
